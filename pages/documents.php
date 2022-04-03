6fe5e3e8549e3f7bf3bb352a339d151b9b66fa71
tashenoff <kail.dead.kd@gmail.com>
2022-04-03 15:10:22 +0600

    first commit

--- pages/documents.php
+++ pages/documents.php
@@ -1,17 +1,96 @@
 <?php
+
 /**
-* Template Name: documents
-*
-* @package WordPress
-*/
+ * Template Name: documents
+ *
+ * @package WordPress
+ */
 ?>
 
 <?php get_header(); ?>
-<?php get_template_part( 'parts/home/banner'); ?> 
-<?php get_template_part( 'parts/home/about'); ?> 
-<?php// get_template_part( 'parts/home/support'); ?> 
+
+<section style="background-repeat:no-repeat; background-size:cover; background-image: url(<?php echo the_post_thumbnail_url(); ?>);" class="bg-primary text-white">
+    <div class="container mx-auto">
+        <div class="flex flex-col justify-center items-start  h-[643px]">
+            <?php the_title(sprintf('<h1 class="headers"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h1>'); ?>
+
+
+        </div>
+    </div>
+</section>
+
+<? php // get_template_part( 'parts/home/support'); 
+?>
+
+
+<div class="container">
+    <div class="flex w-full">
+    <div class="w-full relative">
+    <div class="bg-primary h-[50px] w-[50px] block relative -top-40 right-0">1</div>
+        <ul>
+            <li>Письмо авторизации</li>
+            <li>Письмо авторизации</li>
+            <li>Письмо авторизации</li>
+            <li>Письмо авторизации</li>
+            <li>Письмо авторизации</li>
+        </ul>
+
+      
+    </div>
+    <div class="div relative -top-40">
+
+    <div style="width:600px " class="swiper mySwiper-doc">
+        <div class="swiper-wrapper">
+            <div style="" class="swiper-slide  w-[200px]"><img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
+            <div style="" class="swiper-slide  w-[200px]"> <img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
+            <div style="" class="swiper-slide  w-[200px]"><img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
+            <div style="" class="swiper-slide  w-[200px]"><img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
+
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+
+    </div>
+    </div>
+    
+</div>
 
 
 
+<div style="" class="">
+    <div class="flex items-end w-full">
+    <!-- Swiper -->
+    <!-- <div style="width:500px;" class="swiper mySwiper-doc">
+        <div class="swiper-wrapper">
+            <div style="width:500px;" class="swiper-slide  w-[200px]"><img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
+            <div style="width:500px;" class="swiper-slide  w-[200px]"> <img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
+            <div style="width:500px;" class="swiper-slide  w-[200px]"><img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
+            <div style="width:500px;" class="swiper-slide  w-[200px]"><img class="px-5 " src="<?php echo get_template_directory_uri(); ?>/resources/img/slider.png" alt=""></div>
 
-<?php get_footer();?>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div> -->
+</div>
+</div>
+
+<script>
+    var swiper = new Swiper(".mySwiper-doc", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+</script>
+
+<?php get_footer(); ?>
--- parts/home/about.php
+++ parts/home/about.php
@@ -11,17 +11,20 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/resources/img/home-1.png" alt="">
             </div>
             <div>
-                <p>
+                <p class="p-5">
                     многовекторная компания предоставляющая эффективные и комплексные IT решения. Наша компания
                     предоставляет большой спектр услуг от разработки инфраструктуры, разработки, внедрения и технической
                     поддержки программного обеспечения до продаж инженерных решений, а такжезанимается интеграцией
                     контакт центра от мирового лидера компании Genesys.</p>
             </div>
-            <div>Вести и наблюдать за каждым аспектом Ваших
-                корпоративных IT может быть реальным испытанием,
-                отвлекающим Вас от основных задач Вашего бизнеса.
-                Вы убедитесь в том, что NJForce является значимым
-                активом в Вашей IT стратегии.</div>
+            <div>
+                <p class="p-5"> Вести и наблюдать за каждым аспектом Ваших
+                    корпоративных IT может быть реальным испытанием,
+                    отвлекающим Вас от основных задач Вашего бизнеса.
+                    Вы убедитесь в том, что NJForce является значимым
+                    активом в Вашей IT стратегии.
+                </p>
+            </div>
             <div>
                 <img src="<?php echo get_template_directory_uri(); ?>/resources/img/home-2.png" alt="">
             </div>
@@ -40,16 +43,14 @@
             <p class="py-5">Компания на рынке
                 с 2015 года</p>
         </div>
-        <div class="flex flex-col text-center items-center"><i
-                class="icon text-primary icon-people-tower text-[80px]"></i>
+        <div class="flex flex-col text-center items-center"><i class="icon text-primary icon-people-tower text-[80px]"></i>
             <p class="py-5"> Корпоративные
                 клиенты</p>
         </div>
         <div class="flex flex-col text-center items-center"><i class="icon text-primary icon-network text-[80px]"></i>
             <p class="py-5"> Опытный штат сотрудников</p>
         </div>
-        <div class="flex flex-col text-center items-center"><i
-                class="icon text-primary icon-document-stamp text-[80px]"></i>
+        <div class="flex flex-col text-center items-center"><i class="icon text-primary icon-document-stamp text-[80px]"></i>
             <p class="py-5"> Авторизация от ведущих вендоров</p>
         </div>
     </div>
